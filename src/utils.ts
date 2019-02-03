ff58cd68c99542af1a536fdc58b28b9d40f60fe8
Jonathan Li <jonli@secondspectrum.com>
2019-02-03 05:32:53 -0800

    Add util func for playing lands with conditions

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
 import { Land, Spell, Card } from './deck';
 
+function isLand(card: Card): card is Land {
+  return (card as Land).type !== undefined;
+}
+
 export function getLands(cards: Card[]): Land[] {
   const lands = [];
 
@@ -14,6 +18,20 @@ export function getCMC(spell: Spell): number {
   return spell.costs.reduce((total, idx) => idx.amount + total, 0);
 }
 
-function isLand(card: Card): card is Land {
-  return (card as Land).type !== undefined;
+export function playLandUntapped(landInHand: Land, landsInPlay: Land[]): boolean {
+  if (!landInHand.condition) return true;
+  if (typeof landInHand.condition === 'number') return true;
+
+  const landTypeSet = new Set();
+
+  for (const land of landsInPlay) {
+    for (const landType of land.type) {
+      landTypeSet.add(landType);
+    }
+  }
+
+  for (const cond of landInHand.condition) {
+    if (landTypeSet.has(cond)) return true;
+  }
+  return false;
 }
